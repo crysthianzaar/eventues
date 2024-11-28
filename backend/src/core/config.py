3652e38ca44f675c3b56c788dc18537bee04fac0
Crysthian Zanote Armini <crysthianzanote@gmail.com>
2024-11-27 23:36:46 -0300

    remove old code

--- backend/src/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = os.getenv("DATABASE_URL",
-                         "mysql://root:password@localhost/eventues")
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- backend/src/models/payment_model.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String
-
-
-class PaymentModel(Base):
-    __tablename__ = 'payments'
-    id = Column(Integer, primary_key=True)
-    event_id = Column(Integer, ForeignKey('events.id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
