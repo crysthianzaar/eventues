0dffd972e17704adeae09da43df38214757acc78
Crysthian Zanote Armini <crysthianzanote@gmail.com>
2025-01-28 21:52:57 -0300

    change config next

--- eventues-frontend/next.config.js
+++ eventues-frontend/next.config.js
@@ -1,4 +1,18 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://obc5v0hl83.execute-api.sa-east-1.amazonaws.com" },
+          { key: "Access-Control-Allow-Methods", value: "GET, OPTIONS, PATCH, DELETE, POST, PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
@@ -8,3 +22,5 @@ module.exports = {
     ],
   },
 };
+
+module.exports = nextConfig;
